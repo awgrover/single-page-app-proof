27a12405cb0097af14b1b370624fc274e2691821
Alan Grover <awgrover@gmail.com>
2016-03-12 20:12:46 -0500

    sync with couchdb works

--- app.js
+++ app.js
@@ -3,6 +3,52 @@ console.log("js loading");
 // the dbname is the .html's name
 var dbname= location.pathname.split(/[\\/]/).pop().replace(/\.[^.]+$/,'');
 var db = new PouchDB(dbname);
+var data_peer_sync;
+
+function start_sync(url, db, status_id) {
+  // Will create the remote db if it doesn't exist, if the url has permissions
+  // FIXME: deal with authentication cases
+  console.log("Start sync of "+db+" with "+url);
+  var status_element = $('#'+status_id); // meaningful states
+  var status_element_b = $('#'+status_id+"_b"); // sub-state info
+
+  // we assume our db-name is the same as the remote's. should be configurable
+  // FIXME: we don't defend against doing this twice
+  console.log("remote sync: "+url+'/'+db);
+  data_peer_sync = PouchDB.replicate(db, url+'/'+db, {
+    live: true, // keep running
+    retry: true
+  }).on('change', function (info) {
+    // I think this means "want to change some stuff in or out"
+    console.log("Change",info);
+    // FIXME: display more detail
+    status_element.text("=change "+info.docs_read+"/"+info.docs_written+" "+(new Date()).toISOString()+"=");
+    // FIXME: AND update the displayed data
+  }).on('paused', function (err) {
+    // replication paused (e.g. replication up to date, user went offline)
+    console.log("Paused",err);
+    status_element_b.text("=pause "+(new Date()).toISOString()+"=");
+  }).on('active', function () {
+    // replicate resumed (e.g. new changes replicating, user went back online)
+    console.log("Resumed");
+    status_element_b.text("=resume "+(new Date()).toISOString()+"=");
+  }).on('denied', function (err) {
+    // a document failed to replicate, e.g. due to permissions
+    // FIXME: probably display more info
+    console.log("Denied",err);
+    status_element_b.text("=deny "+(new Date()).toISOString()+"=");
+  }).on('complete', function (info) {
+    // handle complete
+    console.log("Complete",info);
+    // FIXME: display more detail
+    status_element.text("=complete "+info.docs_read+"/"+info.docs_written+" "+info.status+" "+(new Date()).toISOString()+"=");
+  }).on('error', function (err) {
+    // handle error
+    console.log("Error",err);
+    // FIXME: display more detail
+    status_element.text("=error "+(new Date()).toISOString()+"=");
+  });
+  }
 
 function catch_pouch_error(err, result) {
   if (!err) {
@@ -200,6 +246,13 @@ $(function() {
     db.destroy(dbname, catch_pouch_error);
     db = new PouchDB(dbname);
     console.log("whacked db");
+    });
+
+  $('#data_peer_button').click(function() { start_sync( $('#data_peer').val(), dbname, 'data_peer_status'); });
+  
+  $('#data_peer_stop').click( function() {
+    console.log("user cancel data-peer sync");
+    data_peer_sync.cancel();
     });
 
   record_origin();
